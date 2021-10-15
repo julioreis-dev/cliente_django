9995f641c1c4d10cd9ca964c717152801277e304
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2021-10-14 22:47:23 -0300

    fix the file test_turmas_modelto pep8 pattern

--- secretaria/tests/test_turmas_model.py
+++ secretaria/tests/test_turmas_model.py
@@ -2,12 +2,13 @@ from django.test import TestCase
 from secretaria.models import Turma
 from model_mommy import mommy
 
+
 class TurmaModelsTest(TestCase):
     def setUp(self) -> None:
         self.turma = mommy.make('Turma')
 
     def test_turma_exist(self) -> None:
-        turma= Turma.objects.first()
+        turma = Turma.objects.first()
         self.assertIsNotNone(turma)
 
     def test_str_turma(self) -> None:
